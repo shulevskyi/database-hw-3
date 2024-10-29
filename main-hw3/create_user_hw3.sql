5de2cb239e3d60ea72d31521e1008d5a47dce840
shulevskyi <dshulevskyi@kse.org.ua>
2024-10-29 13:12:17 +0200

    update

--- main-hw3/create_user_hw3.sql
+++ main-hw3/create_user_hw3.sql
@@ -1,5 +1,5 @@
 USE ecommerce_shop;
 
 -- Create user and grant privileges
-CREATE USER 'ecom_user'@'localhost' IDENTIFIED BY 'password123';
+CREATE USER 'ecom_user'@'localhost' IDENTIFIED BY 'fifadg13';
 GRANT SELECT, INSERT, UPDATE, DELETE ON ecommerce_shop.* TO 'ecom_user'@'localhost';
