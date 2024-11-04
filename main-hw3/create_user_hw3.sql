546db989a6ab1a2e51792d19965aa082051fcbb5
ADatsiv <119421283+andruto100@users.noreply.github.com>
2024-11-04 18:08:01 +0200

    Update create_user_hw3.sql

--- main-hw3/create_user_hw3.sql
+++ main-hw3/create_user_hw3.sql
@@ -5,10 +5,6 @@ CREATE USER 'ecom_user'@'localhost' IDENTIFIED BY 'fifadg13';
 GRANT SELECT, INSERT, UPDATE, DELETE ON ecommerce_shop.* TO 'ecom_user'@'localhost';
 FLUSH PRIVILEGES;
 
-
-ALTER USER 'ecom_user'@'localhost' IDENTIFIED BY 'fifadg13';
-FLUSH PRIVILEGES;
-
 CREATE USER 'data_analyst'@'localhost' IDENTIFIED BY 'analyst_password123';
 GRANT SELECT ON ecommerce_shop.* TO 'data_analyst'@'localhost';
 FLUSH PRIVILEGES;
